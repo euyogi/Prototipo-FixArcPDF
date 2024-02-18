05be5548463a80d54c2ece3acc8fb15dd2074ca8
yogi <yogiqhz@gmail.com>
2024-02-18 14:23:37 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <windows.h>
+#include <thread>
+#include <chrono>
 
 int main(int argc, char* argv[]) {
     ShowWindow( GetConsoleWindow(), SW_HIDE ); // Esconde o console.
@@ -8,12 +10,11 @@ int main(int argc, char* argv[]) {
     if (argc != 2)
         return 1;
 
-    system("start /min /b python -m http.server -b 127.0.0.1 -d /"); // Inicia um servidor local.
+    system("start /min /b pythonw -m http.server -b 127.0.0.1 -d /"); // Inicia um servidor local.
 
     std::string file = argv[1]; // Diretório do arquivo.
     std::string command = "start /min /b \"\" \"http://127.0.0.1:8000/" + file + "\"";
 
     system(command.c_str()); // Abre o arquivo pelo servidor.
-
-    system("taskkill /IM python3.12.exe"); // Encerra o servidor.
+    system("taskkill /IM pythonw3.12.exe /F"); // Encerra o servidor.
 }
